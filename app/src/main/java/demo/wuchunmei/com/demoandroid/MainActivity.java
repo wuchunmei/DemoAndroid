069d159595218105a0756be398d8a0281fe14e89
wcm <wucm@staff.tianya.cn>
2018-05-02 00:37:38 -0300

    项目初始化提交

---
+++ app/src/main/java/demo/wuchunmei/com/demoandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.wuchunmei.com.demoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/demo/wuchunmei/com/net/RetrofitWrapper.java
@@ -0,0 +1,8 @@
+package demo.wuchunmei.com.net;
+
+/**
+ * 对retrofit了做封装
+ */
+public enum RetrofitWrapper {
+
+}
